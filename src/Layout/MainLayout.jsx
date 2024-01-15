b0b5d5206e22833bf42008ef06f36cba1a448322
mdimranhossainwd <mdimranhossain7498@gmail.com>
2024-01-15 19:23:23 +0600

    Footer Section Completed !

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -1,4 +1,5 @@
 import Banner from "../Components/Banner/Banner";
+import Footer from "../Components/Footer/Footer";
 import Header from "../Components/Header/Header";
 
 const MainLayout = () => {
@@ -6,6 +7,7 @@ const MainLayout = () => {
     <div>
       <Header />
       <Banner />
+      <Footer />
     </div>
   );
 };
