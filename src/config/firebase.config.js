2ac4500067dd896dbf975d444b9d55cf0f3f0b6b
mdimranhossainwd <mdimranhossain7498@gmail.com>
2024-01-15 19:11:33 +0600

    Firebase Config installed and pushed git add .

---
+++ src/config/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBb9AgBr6bO0_txlKgeW6fdpEIykRn4xwY",
+  authDomain: "edubuzz-learning.firebaseapp.com",
+  projectId: "edubuzz-learning",
+  storageBucket: "edubuzz-learning.appspot.com",
+  messagingSenderId: "572300189435",
+  appId: "1:572300189435:web:ff7d8589e19087e0099a5e",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
